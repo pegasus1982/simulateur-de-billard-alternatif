1df311a101d10db1d7b2f2f26238052264d710cd
pegasus1982 <gdm19920108@gmail.com>
2019-08-06 15:23:38 +0800

    initial commit

---
+++ precache-manifest.0449d0237289c5509e4ccdeb184162a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f486510f41a4bc3b6a4f490be822b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34e933c731b153f482f4",
+    "url": "/static/css/main.e182199c.chunk.css"
+  },
+  {
+    "revision": "2c11c94f407af87b7c1c",
+    "url": "/static/js/2.0402486f.chunk.js"
+  },
+  {
+    "revision": "34e933c731b153f482f4",
+    "url": "/static/js/main.924962ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
